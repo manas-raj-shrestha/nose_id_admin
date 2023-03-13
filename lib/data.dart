b3c0ec10916978fe28c5975f17135bd947dadf43
Manas Shrestha <manas.shrestha1992@gmail.com>
2023-03-13 14:31:17 +0545

    First Commit

---
+++ lib/data.dart
@@ -0,0 +1,11 @@
+var data = {
+  '/introduction-screen': ['introduction_screen.jpg'],
+  '/sign-in-screen': ['signin_screen.jpg'],
+  '/sign-up-screen': ['signup_screen.jpg'],
+  '/verification-email-route': ['email_verificaion_screen.jpg'],
+  '/dashboard-screen': ['dashboard_screen.jpg'],
+  '/password-recovery-screen': ['forgot_password_screen.jpg'],
+  '/lost-dog-chooser-screen-route': ['lost_dog_chooser.jpg'],
+  '/verificatioen-email-route': ['email_verificaion_screen.jpg'],
+  '/dashboard-escreen': ['dashboard_screen.jpg'],
+};
---
+++ lib/screenshot_mapper.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:route_mapper/data.dart';
+
+class ScreenshotMapper extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ScreenshotMapperState();
+  }
+}
+
+class _ScreenshotMapperState extends State<ScreenshotMapper> {
+  int selectedRouteIndex = 0;
+  Map<String, List<String>> routeData = data;
+  List<String> keys = data.keys.toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Flexible(
+              flex: 2,
+              child: Container(
+                color: Color(0xff1E3056),
+                child: Column(
+                  children: [_buildHeader(), _buildRoutesListView()],
+                ),
+              )),
+          Flexible(
+            flex: 8,
+            child: Center(
+                child: Image.asset(routeData[keys[selectedRouteIndex]]!.first)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildRoutesListView() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: data.keys.length,
+        itemBuilder: (context, index) {
+          var keys = data.keys;
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 8),
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedRouteIndex = index;
+                  });
+                },
+                child: Text(
+                  keys.toList()[index],
+                  style: TextStyle(color: Colors.white),
+                )),
+          );
+        },
+      ),
+    );
+  }
+
+  _buildHeader() {
+    return Container(
+        padding: EdgeInsets.all(16),
+        child: Text(
+          'Routes',
+          style: TextStyle(color: Colors.white),
+        ));
+  }
+}
